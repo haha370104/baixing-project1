61a41f29256a0fdfb738f46ba993635fa7933a4a
haha370104 <137368440@qq.com>
2016-07-07 23:44:26 +0800

    基本功能完成
    接口完毕
    做好了过滤器
    顺便作了个大死+1s

--- __init__.py
+++ __init__.py
@@ -1,12 +1,29 @@
 from app_config import app, db
-from flask import render_template, request
-from model.employee import employee
+from flask import render_template, request, make_response
 import json
+import time
+from controller.filter import *
+
+
+@app.before_request
+def check_authority():
+    path = request.path
+    if path in filter_list.keys():
+        for fun in filter_list[path]:
+            response = fun(request)
+            if response != None:
+                return response
+        return None
 
 
 @app.route('/')
 def index():
-    return
+    return '吼啊'
+
+
+@app.route('/login/')
+def login():
+    return '江主席,你说先登陆,吼不吼啊'
 
 
 @app.route('/check_login/', methods=['GET', 'POST'])
@@ -17,28 +34,108 @@ def check_login():
     if user != None and user.check(password):
         dic = user.to_json()
         dic['token'] = user.token
-        return json.dumps(dic)
+        res = make_response(json.dumps(dic))
+        res.set_cookie('token', user.token, expires=time.time() + 24 * 60 * 60)
+        session['token'] = user.token
+        return res
     else:
-        return json.dumps({'error_code': '403', 'error_result': '账号不存在或密码错误,请重试'})
+        return json.dumps({'error_code': 404, 'error_result': '账号不存在或密码错误,请重试'})
 
 
 @app.route('/add_staff/', methods=['GET', 'POST'])
 def add_staff():
-    name = request.args['name']
-    phone = request.args['phone']
-    f_department = request.args['f_department']
-    position_title = request.args['position_title']
-    authority_level = request.args['authority_level']
-    position_level = request.args['position_level']
-    emergency_phone = request.args.get('emergency_phone')
-    s_department = request.args.get('s_department')
-    address = request.args.get('address')
-    manager_ID = request.args.get('manager_ID')
-    user = employee(name, phone, f_department, position_title, authority_level, position_level, '000000', emergency_phone,
-                    s_department, address, manager_ID)
-    db.session.add(user)
-    db.session.commit()
+    try:
+        name = request.args['name']
+        phone = request.args['phone']
+        f_department = request.args['f_department']
+        position_title = request.args['position_title']
+        authority_level = request.args['authority_level']
+        position_level = request.args['position_level']
+        emergency_phone = request.args.get('emergency_phone')
+        s_department = request.args.get('s_department')
+        address = request.args.get('address')
+        manager_ID = request.args.get('manager_ID')
+        user = employee(name, phone, f_department, position_title, authority_level, position_level, '000000',
+                        emergency_phone,
+                        s_department, address, manager_ID)
+        db.session.add(user)
+        db.session.commit()
+    except:
+        return json.dumps({'result': 'fail'})
     return json.dumps({'result': 'success'})
+
+
+@app.route('/show_staff/')
+def show_staff():
+    staffs = employee.query.filter_by(s_department=request.args['s_department']).all()
+    result = []
+    for staff in staffs:
+        result.append(staff.to_json())
+    return json.dumps({'result': result})
+
+
+@app.route('/show_team_staff/', methods=['GET'])
+def show_team_staff():
+    staffs = employee.query.filter_by(f_department=request.args['f_department']).all()
+    result = []
+    for staff in staffs:
+        result.append(staff.to_json())
+    return json.dumps({'result': result})
+
+
+@app.route('/show_department_staff/')
+def show_department_staff():
+    staffs = employee.query.all()
+    result = []
+    for staff in staffs:
+        result.append(staff.to_json())
+    return json.dumps({'result': result})
+
+
+@app.route('/edit_staff/', methods=['POST'])
+def edit_staff():
+    edited_staff = employee.query.filter_by(ID=request.args['ID']).first()
+    if (edited_staff == None):
+        return json.dumps({'result': 'No such a staff with posted ID'})
+    detail = json.loads(request.args['detail'])
+    attrs = list(detail.keys())
+    values = list(detail.values())
+    for i in range(len(attrs)):
+        setattr(edited_staff, attrs[i], values[i])
+    db.session.add(edited_staff)
+    db.session.commit()
+    return json.dumps({'result': 'update success'})
+
+
+@app.route('/delete_staff/', methods=['POST'])
+def delete_staff():
+    deleted_staff = employee.query.filter_by(ID=request.args['ID']).first()
+    if (deleted_staff == None):
+        return json.dumps({'result': 'No such a staff with posted ID'})
+    deleted_staff.delete()
+    db.session.commit()
+    return json.dumps({'result': 'delete success'})
+
+
+@app.route('/edit_profile/', methods=['POST'])
+def edit_profile():
+    return edit_staff()
+
+
+@app.route('/change_password/', methods=['POST'])
+def change_password():
+    user = employee.query.filter_by(ID=request.args['ID']).first()
+    user.paswswd = request.args['password']
+    db.session.delete(user)
+    db.session.commit()
+    return json.dumps({'result': 'change password success'})
+
+
+@app.route('/logout/')
+def logout():
+    res = make_response(json.dumps({'result': 'success'}))
+    res.set_cookie('token', '')
+    return res
 
 
 if __name__ == '__main__':
---
+++ controller/filter.py
@@ -0,0 +1,68 @@
+from flask import redirect, session, url_for
+from model.employee import employee
+import json
+
+
+def get_employee_by_token(request):
+    token = request.cookies.get('token')
+    if token != None:
+        user = employee.query.filter_by(token=token).first()
+        return user
+    else:
+        return None
+
+
+def login_again_filter(request):
+    token = request.cookies.get('token')
+    if token != None and token == session.get('token'):
+        return redirect(url_for('index'))
+    elif token != None:
+        user = employee.query.filter_by(token=token).first()
+        if user != None:
+            session['token'] = user.token
+            dic = user.to_json()
+            dic['token'] = user.token
+            return redirect(url_for('index'))
+
+
+def login_filter(request):
+    user = get_employee_by_token(request)
+    if user == None:
+        return redirect(url_for('login'))
+
+
+def read_controll(request):
+    user = get_employee_by_token(request)
+    authority_level = user.authority_level
+    if authority_level == 0 and (request.args.get('f_department') != user.f_department or request.args.get(
+            's_department') != user.s_department):
+        return json.dumps({'error_reason': 'permission denied', 'error_code': 403})
+    if authority_level == 1 and request.args.get('f_department') != user.f_department:
+        return json.dumps({'error_reason': 'permission denied', 'error_code': 403})
+
+
+def change_controll(request):
+    user = get_employee_by_token(request)
+    authority_level = user.authority_level
+    if authority_level != 3:
+        return json.dumps({'error_reason': 'permission denied', 'error_code': 403})
+
+
+def change_self(request):
+    user = get_employee_by_token(request)
+    if user.ID != request.args.get('ID'):
+        return json.dumps({'error_reason': 'permission denied', 'error_code': 403})
+
+
+filter_list = {
+    '/': [login_filter],
+    '/logout/': [login_filter],
+    '/check_login/': [login_again_filter],
+    '/login/': [login_again_filter],
+    '/show_staff/': [login_filter, read_controll],
+    '/show_team_staff/': [login_filter, read_controll],
+    '/show_department_staff/': [login_filter, read_controll],
+    '/add_staff/': [login_filter, change_controll],
+    '/edit_staff/': [login_filter, change_controll],
+    '/delete_staff/': [login_filter, change_controll]
+}
